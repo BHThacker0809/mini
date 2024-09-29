de3a4b1b59d6a1fd87a24de369451ccc9c2099d7
BHThacker0809 <112827517+BHThacker0809@users.noreply.github.com>
2024-09-29 19:15:15 +0200

    Add files via upload

--- injection.js
+++ injection.js
@@ -1,90 +1,214 @@
 let replacements = {};
 let dumpedVarNames = {};
 const storeName = "a" + crypto.randomUUID().replaceAll("-", "").substring(16);
+const vapeName = crypto.randomUUID().replaceAll("-", "").substring(16);
 
 // ANTICHEAT HOOK
 function replaceAndCopyFunction(oldFunc, newFunc) {
-    return new Proxy(oldFunc, {
-        apply(orig, origIden, origArgs) {
-            const result = orig.apply(origIden, origArgs);
-            newFunc(result);
-            return result;
-        },
-        get(orig) { return orig; }
-    });
+	return new Proxy(oldFunc, {
+		apply(orig, origIden, origArgs) {
+			const result = orig.apply(origIden, origArgs);
+			newFunc(result);
+			return result;
+		},
+		get(orig) { return orig; }
+	});
 }
 
 Object.getOwnPropertyNames = replaceAndCopyFunction(Object.getOwnPropertyNames, function(list) {
-    if (list.indexOf(storeName) != -1) list.splice(list.indexOf(storeName), 1);
-    return list;
+	if (list.indexOf(storeName) != -1) list.splice(list.indexOf(storeName), 1);
+	return list;
 });
 Object.getOwnPropertyDescriptors = replaceAndCopyFunction(Object.getOwnPropertyDescriptors, function(list) {
-    delete list[storeName];
-    return list;
+	delete list[storeName];
+	return list;
 });
 
+function addReplacement(replacement, code, replaceit) {
+	replacements[replacement] = [code, replaceit];
+}
+
+function addDump(replacement, code) {
+	dumpedVarNames[replacement] = code;
+}
+
 function modifyCode(text) {
-    for (const [name, regex] of Object.entries(dumpedVarNames)) {
-        const matched = text.match(regex);
-        if (matched) {
-            for (const [replacement, code] of Object.entries(replacements)) {
-                delete replacements[replacement];
-                replacements[replacement.replaceAll(name, matched[1])] = [code[0].replaceAll(name, matched[1]), code[1]];
-            }
-        }
-    }
+	for(const [name, regex] of Object.entries(dumpedVarNames)){
+		const matched = text.match(regex);
+		if (matched) {
+			console.log(name, regex, matched);
+			for(const [replacement, code] of Object.entries(replacements)){
+				delete replacements[replacement];
+				replacements[replacement.replaceAll(name, matched[1])] = [code[0].replaceAll(name, matched[1]), code[1]];
+			}
+		}
+	}
 
-    for (const [replacement, code] of Object.entries(replacements)) {
-        text = text.replaceAll(replacement, code[1] ? code[0] : replacement + code[0]);
-    }
+	for(const [replacement, code] of Object.entries(replacements)){
+		text = text.replaceAll(replacement, code[1] ? code[0] : replacement + code[0]);
+	}
 
-    var newScript = document.createElement("script");
-    newScript.type = "module";
-    newScript.crossOrigin = "";
-    newScript.textContent = text;
-    var head = document.querySelector("head");
-    head.appendChild(newScript);
-    newScript.textContent = "";
-    newScript.remove();
+	var newScript = document.createElement("script");
+	newScript.type = "module";
+	newScript.crossOrigin = "";
+	newScript.textContent = text;
+	var head = document.querySelector("head");
+	head.appendChild(newScript);
+	newScript.textContent = "";
+	newScript.remove();
 }
 
 (function() {
-    'use strict';
+	'use strict';
 
-    function getMoveDirection(moveSpeed) {
-        let moveStrafe = player$1.moveStrafeDump;
-        let moveForward = player$1.moveForwardDump;
-        let speed = moveStrafe * moveStrafe + moveForward * moveForward;
-        if (speed >= 1e-4) {
-            speed = Math.sqrt(speed), speed < 1 && (speed = 1), speed = 1 / speed, moveStrafe = moveStrafe * speed, moveForward = moveForward * speed;
-            const rt = Math.cos(player$1.yaw) * moveSpeed;
-            const nt = -Math.sin(player$1.yaw) * moveSpeed;
-            return new Vector3$1(moveStrafe * rt - moveForward * nt, 0, moveForward * rt + moveStrafe * nt);
-        }
-        return new Vector3$1(0, 0, 0);
-    }
+	// Function to get move direction
+	function getMoveDirection(moveSpeed) {
+		let moveStrafe = player$1.moveStrafeDump;
+		let moveForward = player$1.moveForwardDump;
+		let speed = moveStrafe * moveStrafe + moveForward * moveForward;
+		if (speed >= 1e-4) {
+			speed = Math.sqrt(speed), speed < 1 && (speed = 1), speed = 1 / speed, moveStrafe = moveStrafe * speed, moveForward = moveForward * speed;
+			const rt = Math.cos(player$1.yaw) * moveSpeed;
+			const nt = -Math.sin(player$1.yaw) * moveSpeed;
+			return new Vector3$1(moveStrafe * rt - moveForward * nt, 0, moveForward * rt + moveStrafe * nt);
+		}
+		return new Vector3$1(0, 0, 0);
+	}
 
-    // Fly module
-    let flyvalue, flyvert;
-    const fly = new Module("Fly", function(callback) {
-        if (callback) {
-            tickLoop["Fly"] = function() {
-                const dir = getMoveDirection(0.39);
-                player$1.motion.x = dir.x;
-                player$1.motion.z = dir.z;
-                player$1.motion.y = keyPressedDump("space") ? flyvert[1] : (keyPressedDump("shift") ? -flyvert[1] : 0);
-            };
-        }
-        else {
-            delete tickLoop["Fly"];
-            if (player$1) {
-                player$1.motion.x = Math.max(Math.min(player$1.motion.x, 0.3), -0.3);
-                player$1.motion.z = Math.max(Math.min(player$1.motion.z, 0.3), -0.3);
-            }
-        }
-    });
-    flyvalue = fly.addoption("Speed", Number, 2);
-    flyvert = fly.addoption("Vertical", Number, 0.7);
+	// Fly Module
+	let flyvalue, flyvert, flybypass;
+	const fly = new Module("Fly", function(callback) {
+		if (callback) {
+			let ticks = 0;
+			tickLoop["Fly"] = function() {
+				ticks++;
+				const dir = getMoveDirection(0.39);
+				player$1.motion.x = dir.x;
+				player$1.motion.z = dir.z;
+				player$1.motion.y = keyPressedDump("space") ? flyvert[1] : (keyPressedDump("shift") ? -flyvert[1] : 0);
+			};
+		}
+		else {
+			delete tickLoop["Fly"];
+			if (player$1) {
+				player$1.motion.x = Math.max(Math.min(player$1.motion.x, 0.3), -0.3);
+				player$1.motion.z = Math.max(Math.min(player$1.motion.z, 0.3), -0.3);
+			}
+		}
+	});
+	flybypass = fly.addoption("Bypass", Boolean, true);
+	flyvalue = fly.addoption("Speed", Number, 2);
+	flyvert = fly.addoption("Vertical", Number, 0.7);
 
-    new Module("NoSlowdown", function() {});
+	// Initialize fly module
+	fly.toggle();
+})();
+`);
+
+	async function saveVapeConfig(profile) {
+		if (!loadedConfig) return;
+		let saveList = {};
+		for(const [name, module] of Object.entries(unsafeWindow.globalThis[storeName].modules)) {
+			saveList[name] = {enabled: module.enabled, bind: module.bind, options: {}};
+			for(const [option, setting] of Object.entries(module.options)) {
+				saveList[name].options[option] = setting[1];
+			}
+		}
+		GM_setValue("vapeConfig" + (profile ?? unsafeWindow.globalThis[storeName].profile), JSON.stringify(saveList));
+		GM_setValue("mainVapeConfig", JSON.stringify({profile: unsafeWindow.globalThis[storeName].profile}));
+	};
+
+	async function loadVapeConfig(switched) {
+		loadedConfig = false;
+		const loadedMain = JSON.parse(await GM_getValue("mainVapeConfig", "{}")) ?? {profile: "default"};
+		unsafeWindow.globalThis[storeName].profile = switched ?? loadedMain.profile;
+		const loaded = JSON.parse(await GM_getValue("vapeConfig" + unsafeWindow.globalThis[storeName].profile, "{}"));
+		if (!loaded) {
+			loadedConfig = true;
+			return;
+		}
+
+		for(const [name, module] of Object.entries(loaded)) {
+			const realModule = unsafeWindow.globalThis[storeName].modules[name];
+			if (!realModule) continue;
+			if (realModule.enabled != module.enabled) realModule.toggle();
+			if (realModule.bind != module.bind) realModule.setbind(module.bind);
+			if (module.options) {
+				for(const [option, setting] of Object.entries(module.options)) {
+					const realOption = realModule.options[option];
+					if (!realOption) continue;
+					realOption[1] = setting;
+				}
+			}
+		}
+		loadedConfig = true;
+	};
+
+	async function exportVapeConfig() {
+		navigator.clipboard.writeText(await GM_getValue("vapeConfig" + unsafeWindow.globalThis[storeName].profile, "{}"));
+	};
+
+	async function importVapeConfig() {
+		const arg = await navigator.clipboard.readText();
+		if (!arg) return;
+		GM_setValue("vapeConfig" + unsafeWindow.globalThis[storeName].profile, arg);
+		loadVapeConfig();
+	};
+
+	let loadedConfig = false;
+	async function execute(src, oldScript) {
+		Object.defineProperty(unsafeWindow.globalThis, storeName, {value: {}, enumerable: false});
+		if (oldScript) oldScript.type = 'javascript/blocked';
+		await fetch(src).then(e => e.text()).then(e => modifyCode(e));
+		if (oldScript) oldScript.type = 'module';
+		await new Promise((resolve) => {
+			const loop = setInterval(async function() {
+				if (unsafeWindow.globalThis[storeName].modules) {
+					clearInterval(loop);
+					resolve();
+				}
+			}, 10);
+		});
+		unsafeWindow.globalThis[storeName].saveVapeConfig = saveVapeConfig;
+		unsafeWindow.globalThis[storeName].loadVapeConfig = loadVapeConfig;
+		unsafeWindow.globalThis[storeName].exportVapeConfig = exportVapeConfig;
+		unsafeWindow.globalThis[storeName].importVapeConfig = importVapeConfig;
+		loadVapeConfig();
+		setInterval(async function() {
+			saveVapeConfig();
+		}, 10000);
+	}
+
+	const publicUrl = "scripturl";
+	// https://stackoverflow.com/questions/22141205/intercept-and-alter-a-sites-javascript-using-greasemonkey
+	if (publicUrl == "scripturl") {
+		if (navigator.userAgent.indexOf("Firefox") != -1) {
+			window.addEventListener("beforescriptexecute", function(e) {
+				if (e.target.src.includes("https://miniblox.io/assets/index")) {
+					e.preventDefault();
+					e.stopPropagation();
+					execute(e.target.src);
+				}
+			}, false);
+		}
+		else {
+			new MutationObserver(async (mutations, observer) => {
+				let oldScript = mutations
+					.flatMap(e => [...e.addedNodes])
+					.filter(e => e.tagName == 'SCRIPT')
+					.find(e => e.src.includes("https://miniblox.io/assets/index"));
+
+				if (oldScript) {
+					observer.disconnect();
+					execute(oldScript.src, oldScript);
+				}
+			}).observe(document, {
+				childList: true,
+				subtree: true,
+			});
+		}
+	}
+	else {
+		execute(publicUrl);
+	}
 })();
